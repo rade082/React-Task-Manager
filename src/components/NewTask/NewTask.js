7d43607c3c7e1ceff7b47bdf8cedfbd0eaede2f6
unknown <dutt2630@gmail.com>
2021-09-23 18:06:36 +0530

    Initial Commit

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import useHttp from '../../hooks/use-http';
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+
+const NewTask = (props) => {
+  const {isLoading,error,sendRequest: sendTaskRequest} = useHttp();
+  
+  const createTask = (taskText, taskData) =>{
+    const generatedId = taskData.name;
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  }
+
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest(
+      {url:'https://react-a3893-default-rtdb.firebaseio.com/',
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+    body: {text: taskText}
+  },createTask.bind(null, taskText)
+     ); 
+    };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
